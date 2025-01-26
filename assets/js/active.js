f7e60e33ea993b48e526603ad58327d170c987cc
Ashekur Rahman <ashekurrahman1@gmail.com>
2025-01-26 20:11:04 +0600

    Added all fiels

---
+++ assets/js/active.js
@@ -0,0 +1,40 @@
+;(function($) {
+    $(document).ready(function() {
+        $('.counter').each(function() {
+            var $this = $(this);
+            var countTo = $this.attr('data-count');
+            
+            $({ countNum: $this.text() }).animate({ countNum: countTo }, {
+                duration: 2000, // Duration of the countdown
+                easing: 'swing', // Easing function
+                step: function() {
+                    $this.text(Math.floor(this.countNum));
+                },
+            });
+        });
+    });
+})(jQuery);
+var swiper = new Swiper(".service__slider", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true, // Enable infinite loop
+    autoplay: {
+        delay: 0, // Continuous sliding
+        disableOnInteraction: false, // Continue autoplay after user interaction
+    },
+    speed: 4000, // Slide speed in milliseconds
+    simulateTouch: false, // Disable touch interaction
+    allowTouchMove: false, // Disable slide dragging
+    draggable: false, // Disable dragging on desktop
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 4,
+        },
+        1024: {
+            slidesPerView: 5,
+        },
+    },
+});
